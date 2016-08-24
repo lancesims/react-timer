5ab5e5858800ee088ce3ec4faa285901e4d1a81c
lsims <lsims@homeaway.com>
2016-08-24 12:46:22 -0500

    I like tests, yay

--- app/tests/components/Clock.test.jsx
+++ app/tests/components/Clock.test.jsx
@@ -6,35 +6,38 @@ var TestUtils = require('react-addons-test-utils');
 
 var Clock = require('Clock');
 
-describe('Clock component', () => {
+describe('Clock', () => {
   it('should exist', () => {
     expect(Clock).toExist();
   });
 
+  describe('render', () => {
+    it('should render clock to output', () => {
+      var clock = TestUtils.renderIntoDocument(<Clock totalSeconds={62}/>);
+      var $el = $(ReactDOM.findDOMNode(clock));
+      var actualText = $el.find('.clock-text').text();
+
+      expect(actualText).toBe('01:02');
+    });
+  });
+
   describe('formatSeconds', () => {
     it('should format seconds', () => {
-        let clock = TestUtils.renderIntoDocument(<Clock/>);
-        let seconds = 615;
-        let expected = '10:15';
-        let actual = clock.formatSeconds(seconds);
-        expect(actual).toBe(expected);
-    });
+      var clock = TestUtils.renderIntoDocument(<Clock/>);
+      var seconds = 615;
+      var expected = '10:15';
+      var actual = clock.formatSeconds(seconds);
 
-    it('should format seconds with leding 0', () => {
-      let clock = TestUtils.renderIntoDocument(<Clock/>);
-      let seconds = 61;
-      let expected = '01:01';
-      let actual = clock.formatSeconds(seconds);
       expect(actual).toBe(expected);
     });
 
-    describe('render', () => {
-      it('should render clock to output', () => {
-          var clock = TestUtils.renderIntoDocument(<Clock totalSeconds={62}/>);
-          var $el = $(ReactDOM.findDOMNode(clock));
-          var actualText = el.find('clock-text').text();
-          expect(actualText).toBe('01:02');
-      });
+    it('should format seconds when min/sec are less than 10', () => {
+      var clock = TestUtils.renderIntoDocument(<Clock/>);
+      var seconds = 61;
+      var expected = '01:01';
+      var actual = clock.formatSeconds(seconds);
+
+      expect(actual).toBe(expected);
     });
   });
 });
