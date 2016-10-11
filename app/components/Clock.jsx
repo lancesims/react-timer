a7ec8c055eeba6428fb371ee03a4e1ae35476fbc
lsims <lsims@homeaway.com>
2016-10-11 07:17:33 -0500

    rad new timer

--- app/components/Clock.jsx
+++ app/components/Clock.jsx
@@ -1,35 +1,35 @@
 var React = require('react');
 
 var Clock = React.createClass({
-  getDefaultProps: () => {
-    totalSeconds: 0
-  },
-  propTypes: {
-    totalSecons: React.PropTypes.number
-  },
-  formatSeconds: (totalSeconds) => {
-    let seconds = totalSeconds % 60;
-    let minutes = Math.floor(totalSeconds/60);
+    getDefaultProps: () => {
+        totalSeconds: 0
+    },
+    propTypes: {
+        totalSeconds: React.PropTypes.number
+    },
+    formatSeconds: (totalSeconds) => {
+        let seconds = totalSeconds % 60;
+        let minutes = Math.floor(totalSeconds / 60);
 
-    if(seconds < 10) {
-      seconds = '0' + seconds;
-    }
+        if (seconds < 10) {
+            seconds = '0' + seconds;
+        }
 
-    if(minutes < 10) {
-      minutes = '0' + minutes;
-    }
-    return minutes + ':' + seconds;
-  },
-  render: function() {
-    var {totalSeconds} = this.props;
-    return (
-      <div className="clock">
+        if (minutes < 10) {
+            minutes = '0' + minutes;
+        }
+        return minutes + ':' + seconds;
+    },
+    render: function () {
+        var {totalSeconds} = this.props;
+        return (
+            <div className="clock">
         <span className="clock-text">
           {this.formatSeconds(totalSeconds)}
         </span>
-      </div>
-    )
-  }
+            </div>
+        )
+    }
 });
 
 module.exports = Clock;
